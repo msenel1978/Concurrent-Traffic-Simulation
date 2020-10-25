01f877c6aff4974e54842dad69096f87c7f54d01
Murat Senel <msenel@gmail.com>
2020-10-25 16:09:47 -0400

    TrafficLight: Define MessageQueue class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -12,7 +12,7 @@ enum class TrafficLightPhase { red, green };
 class Vehicle;
 
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
+// FP.3-DONE Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
@@ -21,8 +21,13 @@ template <class T>
 class MessageQueue
 {
 public:
+	void send(TrafficLightPhase &&);
+	TrafficLightPhase receive();
 
 private:
+	std::condition_variable _queueCond;
+	std::mutex _mutex;
+	std::deque<TrafficLightPhase> _queue;
     
 };
 
