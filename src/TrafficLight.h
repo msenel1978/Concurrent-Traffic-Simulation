47399d9425c6d7ed5696c80493b6451d4b36a9e8
Murat Senel <msenel@gmail.com>
2020-10-25 10:06:04 -0400

    Correct the private member _messageQueue in TrafficLight class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -50,6 +50,7 @@ private:
     void cycleThroughPhases();
 
     TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _messageQueue;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
