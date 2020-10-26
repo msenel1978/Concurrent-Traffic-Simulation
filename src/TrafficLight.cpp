892035340db4fedb9cc35992a2186cdca7a63940
Murat Senel <msenel@gmail.com>
2020-10-26 06:42:36 -0400

    Intersection: Add TrafficLight and add relevant functions

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -9,7 +9,7 @@
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // FP.5a-DONE : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function.
 
@@ -50,7 +50,7 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // FP.5b-DONE : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
     TrafficLightPhase light_phase;
